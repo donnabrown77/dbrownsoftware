875f85939ff2e77c756544b4ed2aa14f566e20b2
dbrown77 <donna.brown05@gmail.com>
2023-11-28 12:58:37 -0800

    gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     appDir: true,
     // https://stackoverflow.com/questions/75571651/using-remark-and-rehype-plugins-with-nextjs-13/75571708#75571708
